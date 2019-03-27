7ef62642991350c1127cf98407fe9c2eedf9fcd4
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-03-26 19:57:09 -0500

    Fixed pinning position

--- BUEarth 2.0/Assets/Scripts/PinScript.cs
+++ BUEarth 2.0/Assets/Scripts/PinScript.cs
@@ -9,18 +9,17 @@ public class PinScript : MonoBehaviour
     {        
         // Do a raycast into the world that will only hit the Spatial Mapping mesh.
         var gazeDirection = Camera.main.transform.forward;
+        this.transform.position = Camera.main.transform.position + gazeDirection.normalized * 11;
 
-       
 
         AudioLibrary.AlertSound();
         // Move this object's parent object to
         // where the raycast hit the Spatial Mapping mesh.
-        this.transform.position = gazeDirection.normalized * 11;
+        
 
         // Rotate this object's parent object to face the user.
         Quaternion toQuat = Quaternion.LookRotation(Camera.main.transform.forward);
-        // toQuat.x = 0;
-        //toQuat.z = 0;
+
         this.transform.parent.rotation = toQuat;
     }
 }
