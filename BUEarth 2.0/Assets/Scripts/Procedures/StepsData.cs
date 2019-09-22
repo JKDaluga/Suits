8c1ba8d9cc2e7a3c69d7a81f9fba93599ba6ca76
Doug Guzman <dgguzman@mail.bradley.edu>
2019-09-22 11:01:20 -0500

    Delets two ununed scripts

--- BUEarth 2.0/Assets/Scripts/Procedures/StepsData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[System.Serializable]
-public class StepsData{
-    public string instruction;
-    public string tip;
-    public string drawing;
-}
